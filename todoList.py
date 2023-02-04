0afe7ba0729871e2e2635427277142433033bf46
gustavo <gu.almeidan2007@gmail.com>
2023-02-04 09:25:19 -0300

    remover tarefas da lista

--- todoList.py
+++ todoList.py
@@ -1,4 +1,4 @@
-todoList = []
+todoList = [""]
 
 rodando = True
 
@@ -12,10 +12,17 @@ while rodando == True:
     todoList.append(task)
     
     #show list
-    print ("\nSuas tarefas são:\n",todoList,"\n\ndigite 'sair' para parar o programa\n")
+    print ("\nSuas tarefas são:\n",todoList,"\n\ndigite 'sair' para parar o programa\ndigite 'remover' para remover uma tarefa\n")
     
     # QUIT
     if task == "sair":
         rodando = False
+        todoList.remove("sair")
+    elif task == "remover":
+        removeTask = input('qual tarefa voce deseja remover?\n')
+        todoList.remove(removeTask)
+        todoList.remove('remover')
+        print('todoList')
+        removeTask = ""
 
 #basico...
